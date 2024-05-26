d03b71e0cedd13efa51f33ef562903eaf93d3118
Néstor Ezquerra <nestorezquerra02@gmail.com>
2024-05-26 21:08:44 +0200

    Changed file a little bit

--- src/main/java/com/gammamicroscopii/mixin/OverworldBiomeCreatorMixin.java
+++ src/main/java/com/gammamicroscopii/mixin/OverworldBiomeCreatorMixin.java
@@ -1,5 +1,6 @@
 package com.gammamicroscopii.mixin;
 
+import com.gammamicroscopii.DynamicSeasons;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.sound.BiomeMoodSound;
 import net.minecraft.sound.MusicSound;
@@ -27,16 +28,14 @@ public class OverworldBiomeCreatorMixin {
 
 	@Inject(method = "createSwamp", at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
 	public void eraseSwampSpecialGrassColorRules(RegistryEntryLookup<PlacedFeature> featureLookup, RegistryEntryLookup<ConfiguredCarver<?>> carverLookup, Consumer<SpawnSettings.Builder> additionalSpawnsAdder, CallbackInfoReturnable<Biome> cir, SpawnSettings.Builder builder, GenerationSettings.LookupBackedBuilder lookupBackedBuilder, MusicSound musicSound) {
-		cir.cancel();
-		//cir.setReturnValue(new Biome.Builder().precipitation(true).temperature(0.8f).downfall(0.9f).effects(new BiomeEffects.Builder().waterColor(6388580).waterFogColor(2302743).fogColor(12638463).skyColor(OverworldBiomeCreator.getSkyColor(0.8f)).foliageColor(6975545).grassColor(5382182).moodSound(BiomeMoodSound.CAVE).music(musicSound).build()).spawnSettings(builder.build()).generationSettings(lookupBackedBuilder.build()).build());
-		cir.cancel();
-
+		DynamicSeasons.LOGGER.info("SWAMP METHOD WORKS"); //doesn't show up
+		cir.setReturnValue(new Biome.Builder().precipitation(true).temperature(0.8f).downfall(0.9f).effects(new BiomeEffects.Builder().waterColor(6388580).waterFogColor(2302743).fogColor(12638463).skyColor(OverworldBiomeCreator.getSkyColor(0.8f)).foliageColor(6975545).grassColor(5382182).moodSound(BiomeMoodSound.CAVE).music(musicSound).build()).spawnSettings(builder.build()).generationSettings(lookupBackedBuilder.build()).build());
 	}
 
 	@Inject(method = "createMangroveSwamp", at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
 	public void eraseMangroveSwampSpecialGrassColorRules(RegistryEntryLookup<PlacedFeature> featureLookup, RegistryEntryLookup<ConfiguredCarver<?>> carverLookup, Consumer<SpawnSettings.Builder> additionalSpawnsAdder, CallbackInfoReturnable<Biome> cir, SpawnSettings.Builder builder, GenerationSettings.LookupBackedBuilder lookupBackedBuilder, MusicSound musicSound) {
+		DynamicSeasons.LOGGER.info("MANGROVE SWAMP METHOD WORKS");
 		cir.setReturnValue(new Biome.Builder().precipitation(true).temperature(0.8f).downfall(0.9f).effects(new BiomeEffects.Builder().waterColor(3832426).waterFogColor(5077600).fogColor(12638463).skyColor(OverworldBiomeCreator.getSkyColor(0.8f)).foliageColor(9285927).grassColor(5382182).moodSound(BiomeMoodSound.CAVE).music(musicSound).build()).spawnSettings(builder.build()).generationSettings(lookupBackedBuilder.build()).build());
-		cir.cancel();
 	}
 
 }
